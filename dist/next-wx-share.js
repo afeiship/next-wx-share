7915f4e2814d0ebf3865ac49dd220219f4a40eff
afeiship <1290657123@qq.com>
2018-01-21 19:22:05 +0800

    issue: wrap share with ready

--- dist/next-wx-share.js
+++ dist/next-wx-share.js
@@ -7,23 +7,27 @@
   var NxWxShare = nx.declare('nx.WxShare', {
     statics: {
       share: function( inTypes, inOptions ) {
-        inTypes.forEach(function (item) {
-          return new Promise(function(resove, reject){
-            wx[item](
-              nx.mix( inOptions, {
-                success: function( data ){
-                  resove( { status:'success', data: data} );
-                },
-                fail: function( data ){
-                  resove( { status:'fail', data: data} );
-                },
-                complete: function( data ){
-                  resolve( { status:'complete', data: data } )
-                }
-              })
-            )
+
+        wx.ready(function () {
+          inTypes.forEach(function (item) {
+            return new Promise(function(resove, reject){
+              wx[item](
+                nx.mix( inOptions, {
+                  success: function( data ){
+                    resove( { status:'success', data: data} );
+                  },
+                  fail: function( data ){
+                    resove( { status:'fail', data: data} );
+                  },
+                  complete: function( data ){
+                    resolve( { status:'complete', data: data } )
+                  }
+                })
+              )
+            });
           });
         });
+
       }
     }
   });
