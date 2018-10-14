f8cd3b0563c9a11e651b801210f8329068b45672
afeiship <1290657123@qq.com>
2018-10-14 20:10:51 +0800

    issue: context & version update

--- dist/next-wx-share.js
+++ dist/next-wx-share.js
@@ -1,37 +1,37 @@
-(function () {
-
-  var global = global || this || self || window;
+(function() {
+  var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
-  var wx = global.wx = global.wx;
+  var wx = (global.wx = global.wx);
 
   var NxWxShare = nx.declare('nx.WxShare', {
     statics: {
-      share: function( inTypes, inOptions ) {
-
-        wx.ready(function () {
-          inTypes.forEach(function (item) {
-            return new Promise(function(resolve, _){
+      share: function(inTypes, inOptions) {
+        inTypes.forEach(function(item) {
+          return new Promise(function(resolve, reject) {
+            if (global.__WEIXIN_READY___) {
               wx[item](
-                nx.mix( inOptions, {
-                  success: function( data ){
-                    resolve( { status:'success', data: data} );
+                nx.mix(inOptions, {
+                  success: function(data) {
+                    resolve({ status: 'success', data: data });
                   },
-                  cancel: function( data ){
-                    resolve( { status:'cancel', data: data} );
+                  cancel: function(data) {
+                    resolve({ status: 'cancel', data: data });
                   }
                 })
-              )
-            });
+              );
+            } else {
+              reject({
+                status: 'fail',
+                msg: '微信SDK还未初始化'
+              });
+            }
           });
         });
-
       }
     }
   });
 
-
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = NxWxShare;
   }
-
-}());
+})();
